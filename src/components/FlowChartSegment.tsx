767423f8faebb2bec6895fbaaa2a568854709e7a
Your Name <you@example.com>
2023-08-21 21:11:42 +0200

    mostly nice

--- src/components/FlowChartSegment.tsx
+++ src/components/FlowChartSegment.tsx
@@ -12,21 +12,20 @@ interface Props {
 
 export const FlowChartSegment = (props: Props) => {
   return (
-    <div class="main-grid mb-2">
-      <h4 class="p-1 border-2 border-gray-500 bg-gray-800 col-start-3 relative w-full">
+    <div class="main-grid my-4">
+      <h4 class="grid p-1 border-2 border-gray-500 bg-gray-800 col-start-3 relative w-full">
         {props.skill.name}
       </h4>
       {props.skill?.children && (
         <For each={props.skill.children}>
           {(item, index) => {
             const isLeft = index() % 2 === 0;
-            const colnr = isLeft ? 1 : 5;
-            const rownr = Math.ceil((index() + 1) / 2);
+            const colnr = isLeft ? "col-start-1" : "col-start-5";
 
             if (!Array.isArray(item.children)) {
               return (
                 <div
-                  class={`p-1 w-full border-2 bg-gray-700 border-gray-500 place- col-start-${colnr} row-start-${rownr}`}
+                  class={`grid p-1 w-full border-2 bg-gray-700 border-gray-500 place- ${colnr} `}
                 >
                   {item.name}
                 </div>
@@ -35,11 +34,9 @@ export const FlowChartSegment = (props: Props) => {
 
             return (
               <div
-                class={`w-full p-1 border-2 bg-slate-800 border-gray-500 text-center group grid cursor-pointer relative col-start-${colnr} row-start-${rownr}`}
+                class={`w-full p-1 border-2 bg-slate-800 border-gray-500 text-center group grid cursor-pointer relative ${colnr} `}
               >
-                <h5 class={`p-1 group-hover text-gray-100 row-start-${rownr}`}>
-                  {item.name}
-                </h5>
+                <h5 class={`p-1 group-hover text-gray-100 `}>{item.name}</h5>
                 <div
                   class={`child-list bg-gray-900  -m-1 transition-opacity duration-500 ease-out opacity-0 group-hover:opacity-100 absolute bg-grey-200  ${
                     isLeft ? "before:left-full" : "before:right-full"
