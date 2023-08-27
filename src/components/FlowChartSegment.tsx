f6c0b6e745275ebc75eb50e2f74711a4739ffd39
Your Name <you@example.com>
2023-08-27 19:20:37 +0200

    cleaning up

--- src/components/FlowChartSegment.tsx
+++ src/components/FlowChartSegment.tsx
@@ -1,6 +1,6 @@
 /** @jsx preserve */
 /** @jsxImportSource solid-js */
-import { For, Index } from "solid-js";
+import { For } from "solid-js";
 
 interface Skill {
   name: string;
@@ -13,7 +13,7 @@ interface Props {
 export const FlowChartSegment = (props: Props) => {
   return (
     <div class="main-grid my-4">
-      <h4 class="lines lines-before lines-after after:w-7 before:w-7 before:top-1/2 after:left-full before:right-full grid p-1 col-start-3 relative w-full">
+      <h4 class="bg-gray-800 border-2 border-gray-500 before:absolute before:border-t-2 before:block before:z-10 before:border-gray-500 after:absolute after:border-t-2 after:block after:z-10 after:top-4 after:border-gray-500 after:w-7 before:w-7 before:top-1/2 after:left-full before:right-full grid p-1 col-start-3 relative w-full">
         {props.skill.name}
       </h4>
       {props.skill?.children && (
@@ -25,7 +25,7 @@ export const FlowChartSegment = (props: Props) => {
             if (!Array.isArray(item.children)) {
               return (
                 <div
-                  class={`lines grid p-1 w-full border-2 bg-gray-700 border-gray-500  ${colnr} `}
+                  class={`grid p-1 w-full border-2 bg-gray-700 border-gray-500  ${colnr} `}
                 >
                   {item.name}
                 </div>
@@ -34,11 +34,11 @@ export const FlowChartSegment = (props: Props) => {
 
             return (
               <div
-                class={`lines w-full p-1 text-center group grid cursor-pointer relative ${colnr} `}
+                class={`bg-gray-800 border-2 border-gray-500 w-full p-1 text-center group grid cursor-pointer relative ${colnr} `}
               >
                 <h5 class={`p-1 group-hover text-gray-100 `}>{item.name}</h5>
                 <div
-                  class={`lines lines-before before:w-5 -m-1 transition-opacity duration-500 ease-out opacity-0 group-hover:opacity-100 absolute ${
+                  class={`bg-gray-800 border-2 border-gray-500 before:absolute before:border-t-2 before:block before:z-10 before:top-4 before:border-gray-500 before:w-5 -m-1 transition-opacity duration-500 ease-out opacity-0 group-hover:opacity-100 absolute ${
                     isLeft ? "before:left-full" : "before:right-full"
                   } ${isLeft ? "-left-32" : "-right-32"} top-1 w-28`}
                 >
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [ 'tailwindcss/nesting'],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      dropShadow: {
+        strong: "4px 4px 4px rgba(31, 41, 55, 0.3)",
+      },
+    },
+  },
+  plugins: ["tailwindcss/nesting"],
+};
