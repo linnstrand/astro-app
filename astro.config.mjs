886a7571fb206da955577a3f45d807ffbe040e26
Your Name <you@example.com>
2023-09-04 16:00:36 +0200

    update packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,21 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
-import solidJs from "@astrojs/solid-js";
+import solid from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    // Example: Disable injecting a basic `base.css` import on every page.
-    // Useful if you need to define and/or import your own custom `base.css`.
-    applyBaseStyles: false
-  }), react(), solidJs()]
+  integrations: [
+    tailwind({
+      // Example: Disable injecting a basic `base.css` import on every page.
+      // Useful if you need to define and/or import your own custom `base.css`.
+      applyBaseStyles: false,
+    }),
+    react({
+      include: ["**/react/*"],
+    }),
+    solid({
+      include: ["**/solid/*"],
+    }),
+  ],
 });
