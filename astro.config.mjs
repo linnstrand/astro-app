3db25497c5143bcd261e0f2d9086ac4dc1ca68e5
Your Name <you@example.com>
2023-09-06 10:42:08 +0200

    config settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import solid from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://linnstrand.github.io",
+  base: "/portfolio",
   integrations: [
     tailwind({
       // Example: Disable injecting a basic `base.css` import on every page.
